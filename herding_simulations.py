4cc34ba12684edb54bb949f8bad6d3f346b071fd
candidate1033211 <104312170+candidate1033211@users.noreply.github.com>
2022-04-24 21:58:53 +0100

    Update herding_simulations.py

--- herding_simulations.py
+++ herding_simulations.py
@@ -2,12 +2,12 @@ import random
 from auxilary import bernouli
 
 
-def create_herding_sign_series(p, gamma, length):
+def create_herding_sign_series(p, decay, length):
     counter = 1.01
     weights = []
     sign_series = [bernouli(0.5)] #initialising the first sign
     for i in range(1, length):
-        weights.append(1-(counter ** gamma))
+        weights.append(1-(counter ** decay))
         mother_sign = random.choices(sign_series, weights=weights)[0]
         new_sign = mother_sign * bernouli(p)
         sign_series.append(new_sign)
